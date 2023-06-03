1c11b18199c694e2cd873ab2010b0fe568647fe8
wyj <2405086407@qq.com>
2023-06-03 17:52:43 +0800

    V19

--- V18/src/main/java/com/birdboot/core/BirdBootApplication.java
+++ V18/src/main/java/com/birdboot/core/BirdBootApplication.java
@@ -3,12 +3,17 @@ package com.birdboot.core;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * 项目主启动类
  */
 public class BirdBootApplication {
     //创建私有属性ServerSocket套接字 记为serverSocket
     private ServerSocket serverSocket;
+    //创建线程池
+    private ExecutorService threadPool;
     //创建构造方法
     public BirdBootApplication(){
         try {
@@ -17,6 +22,8 @@ public class BirdBootApplication {
             // 并指定该对象监听的端口号为8088。用于接收客户端的TCP连接请求
             // 这里的8088是一个整数类型的值，代表服务器监听的端口号，客户端需要使用这个端口号来连接服务器。
             serverSocket = new ServerSocket(8088);
+            //线程池实例化,设置容量
+            threadPool = Executors.newFixedThreadPool(50);
             System.out.println("服务端启动完毕!");
         } catch (IOException e) {
             e.printStackTrace();
@@ -36,6 +43,8 @@ public class BirdBootApplication {
                 //ClientHandler对象会启动一个新的线程来处理客户端的请求，这样可以避免阻塞主线程，提高服务器的并发处理能力。
                 //创建ClientHandler(socket)对象,记为handler
                 ClientHandler handler = new ClientHandler(socket);
+                //将handler交给线程池
+                threadPool.execute(handler);
                 //创建Thread(handler)线程对象,记为t
                 Thread t = new Thread(handler);
                 //线程t调用start方法
